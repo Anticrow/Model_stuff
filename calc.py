d2405c11e4e66d09520dd27e04cbc4a5958f46fb
Anticrow <33156406+Anticrow@users.noreply.github.com>
2018-03-29 01:44:35 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import Perceptron
 import numpy as np
-import flask
+from flask import Flask
 
 
 app = Flask(__name__)
